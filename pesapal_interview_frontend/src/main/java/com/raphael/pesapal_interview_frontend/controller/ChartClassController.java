4442d63723820dca2d5087f9e6ecccd7747446c9
Raphael Kariuki <m0ckinjay@yandex.com>
2026-01-15 09:55:52 +0300

    proper naming chart class related files and paths

--- pesapal_interview_frontend/src/main/java/com/raphael/pesapal_interview_frontend/controller/ChartClassController.java
+++ pesapal_interview_frontend/src/main/java/com/raphael/pesapal_interview_frontend/controller/ChartClassController.java
@@ -3,22 +3,19 @@ package com.raphael.pesapal_interview_frontend.controller;
 import com.raphael.pesapal_interview_frontend.dto.ChartClassDTOs;
 import com.raphael.pesapal_interview_frontend.dto.RegisterChartClassRequest;
 import com.raphael.pesapal_interview_frontend.dto.UpdateChartClassRequest;
-import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.*;
 
 @Controller
+@RequestMapping("/chart-class")
 public class ChartClassController {
 
     private final RestTemplate restTemplate = new RestTemplate();
@@ -26,26 +23,26 @@ public class ChartClassController {
 
 
 
-    @GetMapping("/chartClasses")
+    @GetMapping
     public String chartClass(Model  model) {
         var chartClasses = restTemplate.getForObject(url, ChartClassDTOs.ChartClassResponse[].class);
         model.addAttribute("chartClasses", chartClasses);
-        return "ChartClass";
+        return "chart-class";
     }
 
-    @GetMapping("/addChartClass")
+    @GetMapping("/add")
     public String showAddChartClass(RegisterChartClassRequest registerChartClassRequest, Model model) {
         Map<String, String> map = new HashMap<>();
         var classTypes = restTemplate.getForObject(url + "/classTypes", map.getClass());
         model.addAttribute("classTypes", classTypes);
         model.addAttribute("registerChartClassRequest", registerChartClassRequest);
-        return "AddChartClass";
+        return "create-chart-class";
     }
 
-    @PostMapping("/addChartClass")
+    @PostMapping("/add")
     public String addChartClass(RegisterChartClassRequest registerChartClassRequest) {
        var response = restTemplate.postForObject(url, new HashSet<>(List.of(registerChartClassRequest)), ChartClassDTOs.ChartClassResponse[].class);
-       return "redirect:/chartClasses";
+       return "redirect:/chart-class";
     }
 
     @GetMapping("/update/{id}")
@@ -83,13 +80,13 @@ public class ChartClassController {
         if (!response.getStatusCode().is2xxSuccessful()){
             return "redirect:/error";
         }
-        return "redirect:/chartClasses";
+        return "redirect:/chart-class";
 
     }
 
     @GetMapping("/delete/{id}")
     public String deleteChartClass(@PathVariable Long id) {
         restTemplate.delete(url + "?oid=" + id);
-        return "redirect:/chartClasses";
+        return "redirect:/chart-class";
     }
 }
